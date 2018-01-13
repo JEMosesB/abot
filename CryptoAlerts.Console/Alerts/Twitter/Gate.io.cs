23e63f04cd16dbe9fb1bfd78959e32e512681a15
Moses Blehmans <moisey_b@yahoo.com>
2018-01-13 11:04:17 +0000

    small doc updates

--- CryptoAlerts.Console/Alerts/Twitter/Gate.io.cs
+++ CryptoAlerts.Console/Alerts/Twitter/Gate.io.cs
@@ -15,11 +15,17 @@ namespace CryptoAlerts.ConsoleApp.Alerts.Twitter
 /*
 Analysis:
 Gate.io: Very volatile. Wait up to a day (1.5 max) and take what you can. Avg. increase is ether v. small or 80-100%
+Update: Unless its a new token, price increase is almost guaranteed in 1 day - 2 max.
 
 Reaction Time: a few minutes
 Updates: Few times a month.
 
-GTC - 1st time curr, small incr.
+OST - +30% in half a day.
+GNX - 80% in 3/4 day
+MDT - NewToken - Decline.
+DBC - after a day started to increase, 350% in 5 days, then decline
+QLink - +50% in 1 day and a half
+GTC - ???
 DRGN - 100% over 1.5 days, then small decline
 XMR - slight decline (because after big pump)
 ICX - almost no increase, stable
